2f4210effb5790c407f4f0c1c7f57311c50b2831
burakkayya <56073855+burakkayya@users.noreply.github.com>
2022-08-29 19:09:31 +0300

    Generics

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> myList = new MyList<string>();
+            myList.Add("burak");
+
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 1;
+            product1.ProductName = "T-shirt";
+            product1.UnitPrice = 149.90;
+            product1.UnitsInStock = 210;
+
+            Product product2 = new Product { Id = 2, CategoryId = 2, ProductName = "Jean", 
+                UnitPrice = 199.90, UnitsInStock = 80 };
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product2);
+            productManager.Update(product1);
+
+
+
+        }
+    }
+}
