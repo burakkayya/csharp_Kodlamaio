767f9b7951e2c81b3d3d4ec50ecf95af34010557
burakkayya <56073855+burakkayya@users.noreply.github.com>
2022-08-29 16:17:44 +0300

    Proje dosyası ekle.

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 10;
+            int sayi2 = 20;
+
+            int[] dizi = new int[] { 10, 20, 30 };
+            int[] dizi2 = new int[] { 100, 200, 300 };
+
+            dizi = dizi2;
+            dizi2[0] = 999;
+
+
+            Console.WriteLine(dizi[0]);
+        }
+    }
+}
