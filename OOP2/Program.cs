474728974d2f0d03cf486e50bf50cc6c9e2641e3
burakkayya <56073855+burakkayya@users.noreply.github.com>
2022-09-12 22:58:30 +0300

    Polymorpishm, Interface

---
+++ OOP2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Burak";
+            musteri1.Soyadi = "Kaya";
+            musteri1.TcNo = "16596226626";
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "burrockss";
+            musteri2.VergiNo = "659656622";
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri3);
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ıhtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+            List<ILoggerService> loggerServices = new List<ILoggerService>() { fileLoggerService, databaseLoggerService };
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(konutKrediManager,loggerServices);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() { tasitKrediManager, konutKrediManager };
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+    }
+}
